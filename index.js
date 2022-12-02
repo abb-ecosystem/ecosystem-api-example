49518435a4b398e7bbd3b1d985cedd9179b55750
rcastilloc <raul.castillo-cruces@de.abb.com>
2022-12-02 17:15:42 +0100

    removed requirejs comment from index.js

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
  * your application logic in there.
  */
 
-/**
- * When using RequireJS, the script is loaded asynchronously.
- * In many cases, this means that the load event will happen before
- * the script runs, therefore when using  window.addEventListener('load',...
- * the code may be listening for an event that has already happened.
- */
-window.addEventListener('load', async function () {
+window.addEventListener("load", async function () {
   // RWS.setDebug(1, 0)
   fpComponentsEnableLog();
 
@@ -27,9 +21,11 @@ window.addEventListener('load', async function () {
     console.log(`monitorControllerState : ${value}`);
   });
 
-  document.querySelector('#about-api').textContent = API.ECOSYSTEM_LIB_VERSION;
-  document.querySelector('#about-tcomponents').textContent = T_COMPONENTS_BASE_VERSION;
-  document.querySelector('#about-tcomponents-example').textContent = T_COMPONENTS_EXAMPLE_VERSION;
+  document.querySelector("#about-api").textContent = API.ECOSYSTEM_LIB_VERSION;
+  document.querySelector("#about-tcomponents").textContent =
+    T_COMPONENTS_BASE_VERSION;
+  document.querySelector("#about-tcomponents-example").textContent =
+    T_COMPONENTS_EXAMPLE_VERSION;
 
   const app = new App();
   // const app = new Test(document.getElementById('test'));
